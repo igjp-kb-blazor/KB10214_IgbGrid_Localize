f4d7ed2314731b13c0dc437d07efa8e31a256087
jsakamotoIGJP <jsakamoto@infragistics.com>
2023-01-06 15:19:14 +0900

    初回コミット

---
+++ Data/AthletesDataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace KB10214_IgbGrid_Localize.Data;
+
+public class AthletesDataItem
+{
+    public double Id { get; set; }
+    public string? Avatar { get; set; }
+    public string? Position { get; set; }
+    public string? Name { get; set; }
+    public int AthleteNumber { get; set; }
+    public double BeatsPerMinute { get; set; }
+    public double TopSpeed { get; set; }
+    public string? Registered { get; set; }
+    public double TrackProgress { get; set; }
+    public string? CountryFlag { get; set; }
+    public string? CountryName { get; set; }
+}
