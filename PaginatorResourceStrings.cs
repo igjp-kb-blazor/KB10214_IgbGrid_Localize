4c39cbfb1880c53ff62715a4dc9e92cadeadd8bb
jsakamotoIGJP <jsakamoto@infragistics.com>
2024-09-24 15:40:25 +0900

    ページネーターを追加し、且つ、日本語化を適用

---
+++ PaginatorResourceStrings.cs
@@ -0,0 +1,16 @@
+﻿namespace IgniteUI.Blazor.Controls;
+
+#pragma warning disable BL0005 // Component parameter should not be set outside of its component.
+public static class PaginatorResourceStrings
+{
+    public static readonly IgbPaginatorResourceStrings JA = new()
+    {
+        Igx_paginator_label = "ページごとの項目数",
+        Igx_paginator_pager_text = "/",
+        Igx_paginator_first_page_button_text = "最初のページに移動",
+        Igx_paginator_previous_page_button_text = "前のページ",
+        Igx_paginator_last_page_button_text = "最後のページに移動",
+        Igx_paginator_next_page_button_text = "次のページ"
+    };
+}
+#pragma warning restore BL0005
